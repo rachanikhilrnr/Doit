81cdffe513804529d39da92095199321aba6524f
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-27 11:41:15 +0530

    second

---
+++ index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.listen(PORT,() => {
+    console.log(`Server is running in ${PORT}`);
+})
+
+app.get("", (req,res) => {
+    res.json("WELCOME");
+})
