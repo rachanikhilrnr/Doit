e879e75d4e21040decd4341d58d81a7a9bc533ca
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-27 12:01:44 +0530

    mongodb connect

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 const PORT = 5000;
 
+const connect = mongoose.connect("mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err))
+
 app.listen(PORT,() => {
     console.log(`Server is running in ${PORT}`);
 })
